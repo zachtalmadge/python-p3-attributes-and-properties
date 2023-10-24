514d3fcf8d643ebc8d0ee87a002c182d9477049f
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-10-24 17:05:32 -0400

    init

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,31 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+
+    def __init__(self, name="tank", breed="bulldog"):
+        self.name = name
+        self.breed = breed
+    
+    def get_name(self):
+        return self.name
+    
+    def set_name(self, name):
+        if len(name) < 1 or len(name) > 25:
+            print('Name must be string between 1 and 25 characters.')
+            return
+        else:
+            self.name = name
+
+    name = property(get_name, set_name)
+    
+    def get_breed(self):
+        return self.breed
+    
+    def set_breed(self, breed):
+        if breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+            return
+        else:
+            self.breed = breed
+
+    breed = property(get_breed, set_breed)
